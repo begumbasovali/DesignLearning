d5c785885205e07c29369e2e9c217fb8dee723de
Begüm Başovalı <begumbasovali@posta.mu.edu.tr>
2024-03-22 23:32:35 +0300

    DesignLearning
    A basic pizza app page design for beginner Android Developers.

---
+++ DesignLearning/app/src/main/java/com/example/designlearning/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.designlearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Yazılım kısmın R harfi :res klasörünü temsil eder
+        //xml kısmında @ işareti res klasörünü temsil eder
+
+        //Left - Start - Leading
+        //Right - End - Trailing
+    }
+}
